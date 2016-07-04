9a0d1b10dc9e697f33e7457d6a969e9681b38d95
tabuu9 <ericfborges@gmail.com>
2016-07-04 11:05:34 -0400

    1.2.3
    Dragon Realm:
    +Minor tweaks and edits to the intro text

--- dragon.py
+++ dragon.py
@@ -1,44 +1,75 @@
-import random; import time		
+import time; import random; import sys;
+# import for later code
 
-	def displayIntro():
-	    print('You are in a land full of dragons. In front of you,')
-	    print('you see two caves. In one cave, the dragon is friendly')
-	    print('and will share his treasure with you. The other dragon')
-	    print('is greedy and hungry, and will eat you on sight.')
-	    print()
+def longLine():
+	print('_______________________________________________________')
+def wait(delayTime):
+	time.sleep(delayTime)
+req_version = (3,0)
+cur_version = sys.version_info
 
-	def chooseCave():
-	    cave = ''
-	    while cave != '1' and cave != '2':
-	        print('Which cave will you go into? (1 or 2)')
-	        cave = input()
+if cur_version < req_version:
+		print('You are using an unsupported version of Python.')
+		longLine()
+		wait(2)
 
-	    return cave
+		print('Type help() into your interpreter.')
+		longLine()
+		wait(2)
 
-	def checkCave(chosenCave):
-	    print('You approach the cave...')
-	    time.sleep(2)
-	    print('It is dark and spooky...')
-	    time.sleep(2)
-	    print('A large dragon jumps out in front of you! He opens his jaws and...')
-	    print()
-        time.sleep(2)
+		print('If a version before 2.7 appears, then install Python 3.')
+		longLine()
+		wait(2)
 
-	    friendlyCave = random.randint(1, 2)
+		print('Use the Python launcher which comes with Python 3.')
+		longLine()
+		wait(2)
 
-	    if chosenCave == str(friendlyCave):
-	         print('Gives you his treasure!')
-	    else:
-	         print('Gobbles you down in one bite!')
+		print('Use the python3 command on macOS (OS X)')
+		wait(2)
+		print("   ")
+		quit()
+# check for correct version of Python
 
-	playAgain = 'yes'
-	while playAgain == 'yes' or playAgain == 'y':
+def displayIntro():
+	print('You are in a land full of dragons. In front of you,')
+	print('you see two caves inhabited by dragons. One will')
+	print('share a small portion of gold with you, and the')
+	print('other will eat you alive.')
+	print()
 
-        displayIntro()
+def chooseCave():
+	cave = ''
+	while cave != '1' and cave != '2':
+		print('Which cave will you go into? (1 or 2)')
+	cave = input()
 
-	    caveNumber = chooseCave()
+	return cave
 
-	    checkCave(caveNumber)
+def checkCave(chosenCave):
+	print('You approach the cave...')
+	delay(1.5)
+	print('It is dark and spooky...')
+	delay(2)
+	print('A large dragon jumps out in front of you! He opens his jaws and...')
+	print()
+	delay(2)
 
-	    print('Do you want to play again? (yes or no)')
-	    playAgain = input()
+		friendlyCave = random.randint(1, 2)
+
+		if chosenCave == str(friendlyCave):
+		print('Gives you his treasure!')
+		else:
+		print('Gobbles you down in one bite!')
+
+playAgain = 'yes'
+while playAgain == 'yes' or playAgain == 'y':
+
+	displayIntro()
+
+	caveNumber = chooseCave()
+
+	checkCave(caveNumber)
+
+	print('Do you want to play again? (yes or no)')
+	playAgain = input()
--- game.py
+++ game.py
@@ -1,97 +1,102 @@
 import time; import random; import sys;
 # import for later code
 
-def 
-
+def longLine():
+	print('-------------------------------------------------------')
+def wait(delayTime):
+	time.sleep(delayTime)
 req_version = (3,0)
 cur_version = sys.version_info
 
 if cur_version < req_version:
-    print('-------------------------------------------------------')
-    print('You are using an unsupported version of Python.')
-    print('-------------------------------------------------------')
 
-    time.sleep(2)
-    print('Type help() into your interpreter.')
-    print('-------------------------------------------------------')
-    time.sleep(2)
+	print('You are using an unsupported version of Python.')
+	longLine()
+	wait(2)
 
-    print('If a version before 2.7 appears, then install Python 3.')
-    print('-------------------------------------------------------')
+	print('Type help() into your interpreter.')
+	longLine()
+	wait(2)
 
-    print('Use the Python launcher which comes with Python 3.')
-    time.sleep(2)
-    print('-------------------------------------------------------')
+	print('If a version before 2.7 appears, then install Python 3.')
+	longLine()
+	wait(2)
 
-    print('Use the python3 command on macOS (or OS X)')
-    quit()
+	print('Use the Python launcher which comes with Python 3.')
+	longLine()
+	wait(2)
+
+	print('Use the python3 command on macOS (OS X)')
+	wait(2)
+	print("									   ")
+	quit()
+# check for correct version of Python
 
 guessesTaken = 0
-#preparing some of the code for later
-time.sleep(2)
+wait(2)
 
-print('                                          ')
+print('										  ')
 print('Hello there!')
-time.sleep(1)
+wait(1)
 #delay
 
 print('What is your name? (type name)')
 playerName = input()
-time.sleep(2)
+wait(2)
 
 print('Glad to meet you, ' + playerName + '!')
-time.sleep(2)
+wait(2)
 
 number = random.randint(2, 19)
 print('I am thinking of a number between 1 and 20.')
-time.sleep(2)
+wait(2)
 
 guessesLeft = 6 - guessesTaken
 guessesLeft = str(guessesLeft)
-time.sleep(2)
+wait(2)
 
 print('You have ' + guessesLeft + ' guesses left.')
-time.sleep(2)
+wait(2)
 
 while guessesTaken < 6:
-    print('Type in your guess!')
-    guess = input()
-    try:
-        guess = int(guess)
-    except ValueError:
-        print('Oops! Only type in numbers, please!')
-        continue
+	print('Type in your guess!')
+	guess = input()
+	try:
+		guess = int(guess)
+	except ValueError:
+		print('Oops! Only type in numbers, please!')
+		continue
 #Berating the player for not typing in a number
-    guessesTaken = guessesTaken + 1
+	guessesTaken = guessesTaken + 1
 
 
-    if guess > 20:
-        print("You're supposed to guess between 1 and 20, silly!")
+	if guess > 20:
+		print("You're supposed to guess between 1 and 20, silly!")
 
-    if guess < 0:
-        print('The number is not below 0, alright?')
+	if guess < 0:
+		print('The number is not below 0, alright?')
 #berating the player for guessing over the boundaries
 
 
-    if guess < number:
-        print('Too low.')
+	if guess < number:
+		print('Too low.')
 
 
 
-    if guess > number:
-        print ('Too high.')
+	if guess > number:
+		print ('Too high.')
 #feedback for the player
 
-    if guess == number:
-        break
+	if guess == number:
+		break
 #break the feedback loop and start the "YOU WON text"
 
 if guess == number:
-    guessesTaken = str(guessesTaken)
-    print('Good job, ' + playerName + '! You found it in ' + guessesTaken + ' tries!')
+	guessesTaken = str(guessesTaken)
+	print('Good job, ' + playerName + '! You found it in ' + guessesTaken + ' tries!')
 
 if guess != number:
-    number = str(number)
-    print('Sorry! The number was ' + number + '.')
+	number = str(number)
+	print('Sorry! The number was ' + number + '.')
 
 print('Thanks for playing!')
