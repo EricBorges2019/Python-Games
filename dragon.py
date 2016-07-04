a2ddb79abc044f19eebbd5946d9c9f44a4d0b632
tabuu9 <ericfborges@gmail.com>
2016-07-04 12:11:03 -0400

    1.4
    -infinite loop glitch
    -removed 80% of the “outdated Python” warning

--- dragon.py
+++ dragon.py
@@ -2,74 +2,79 @@ import time; import random; import sys;
 # import for later code
 
 def longLine():
-	print('_______________________________________________________')
+    print('_______________________________________________________')
 def wait(delayTime):
-	time.sleep(delayTime)
+    time.sleep(delayTime)
 req_version = (3,0)
 cur_version = sys.version_info
 
 if cur_version < req_version:
-		print('You are using an unsupported version of Python.')
-		longLine()
-		wait(2)
+        print('You are using an unsupported version of Python.')
+        longLine()
+        wait(2)
 
-		print('Type help() into your interpreter.')
-		longLine()
-		wait(2)
+        print('Type help() into your interpreter.')
+        longLine()
+        wait(2)
 
-		print('If a version before 2.7 appears, then install Python 3.')
-		longLine()
-		wait(2)
+        print('If a version before 2.7 appears, then install Python 3.')
+        longLine()
+        wait(2)
 
-		print('Use the Python launcher which comes with Python 3.')
-		longLine()
-		wait(2)
+        print('Use the Python launcher which comes with Python 3.')
+        longLine()
+        wait(2)
 
-		print('Use the python3 command on macOS (OS X)')
-		wait(2)
-		print("   ")
-		quit()
+        print('Use the python3 command on macOS (OS X)')
+        wait(2)
+        print("   ")
+        quit()
 # check for correct version of Python
 
 def displayIntro():
-	print('You are in a land full of dragons. In front of you,')
-	print('you see two caves inhabited by dragons. One will')
-	print('share a small portion of gold with you, and the')
-	print('other will eat you alive.')
-	print()
+    print('You are in a land full of dragons.')
+    wait(2)
+    print('In front of you, there are two caves inhabited by dragons.')
+    wait(2)
+    print('One will share a small portion of gold with you.')
+    wait(2)
+    print('The other will eat you alive.')
+    wait(2)
+    print()
 
 def chooseCave():
-	cave = ''
-	while cave != '1' and cave != '2':
-		print('Which cave will you go into? (1 or 2)')
-	cave = input()
+    cave = ''
+    while cave != '1' and cave != '2':
+        print('Which cave will you go into? (1 or 2)')
+        cave = input()
 
-	return cave
+    return cave
 
 def checkCave(chosenCave):
-	print('You approach the cave...')
-	delay(1.5)
-	print('It is dark and spooky...')
-	delay(2)
-	print('A large dragon jumps out in front of you! He opens his jaws and...')
-	print()
-	delay(2)
+    print('You approach the cave...')
+    wait(2)
+    print('It is dark and spooky...')
+    wait(2)
+    print('A large dragon jumps out in front of you! He opens his jaws and...')
+    print()
+    wait(2)
 
-		friendlyCave = random.randint(1, 2)
+    friendlyCave = random.randint(1, 2)
 
-		if chosenCave == str(friendlyCave):
-		print('Gives you his treasure!')
-		else:
-		print('Gobbles you down in one bite!')
+    if chosenCave == str(friendlyCave):
+                print('Gives you his treasure!')
+    else:
+                print('Gobbles you down in one bite!')
 
 playAgain = 'yes'
 while playAgain == 'yes' or playAgain == 'y':
 
-	displayIntro()
+    displayIntro()
 
-	caveNumber = chooseCave()
+    caveNumber = chooseCave()
 
-	checkCave(caveNumber)
-
-	print('Do you want to play again? (yes or no)')
-	playAgain = input()
+    checkCave(caveNumber)
+    
+    wait(2)
+    print('Do you want to play again? (yes or no)')
+    playAgain = input()
