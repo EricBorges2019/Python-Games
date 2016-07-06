364e1492909e094d897de81af45add797eb7fed5
tabuu9 <ericfborges@gmail.com>
2016-07-06 11:38:00 -0400

    1.7
    Animal Hangman has two more chances
    NewGame is now TicTacToe

--- Game Storage/animalHangman.py
+++ Game Storage/animalHangman.py
@@ -1,4 +1,4 @@
-import time; import random; import sys;
+  import time; import random; import sys;
 # import for later code
 
 def longLine():
@@ -72,7 +72,23 @@ HANGMANPICS = ['''
  /|\  |
  / \  |
       |
-=========''']
+=========''', '''
+
+  +---+
+  |   |
+ [O   |
+ /|\  |
+ / \  |
+      |
+=========''', '''
+
+  +---+
+  |   |
+ [O]  |
+ /|\  |
+ / \  |
+      |
+========='''
 words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra'.split()
 
 def getRandomWord(wordList):
