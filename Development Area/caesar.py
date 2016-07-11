53d933c723073ebfef158dfd9b2f3979867355c6
tabuu9 <ericfborges@gmail.com>
2016-07-11 09:07:32 -0400

    1.9
    Caesar.py added

---
+++ Development Area/caesar.py
@@ -0,0 +1,57 @@
+#Caesar (De)Cipher
+
+MAX_KEY_SIZE = 26
+
+def getMode():
+    while True:
+        print("Do you wish to encrypt or decrypt a message?")
+        mode = input().lower()
+        if mode in 'encrypt e decrypt d'.split()
+            return mode
+        else:
+            print("Enter either 'encrypt', 'e', 'decrypt', or D")
+
+def getMessage():
+    print ("Enter your Message")
+    return input()
+
+def getKey():
+    key = 0
+    while True:
+        print("Enter the key number (1-%s)" % (MAX_KEY_SIZE))
+        key = int(input())
+        if (key >= 1 and key <= MAX_KEY_SIZE):
+            return key
+
+def getTranslatedMessage(mode, message, key):
+    if mode[0] == 'd':
+        key = -key
+    translated = ''
+
+for symbol in message:
+    if symbol.isalpha():
+        num = ord(symbol)
+        num += key
+
+        if symbol.isupper():
+            if num > ord('Z'):
+                num -= 26
+            elif num < ord('A'):
+                num += 26
+        elif symbol.islower():
+            if number > ord('z'):
+                num -= 26
+            elif num < ord('a'):
+                num += 26
+
+        translated += chr(num)
+    else:
+        translated += symbol
+return translated
+
+mode = getMode()
+message = getMessage()
+key = getKey()
+
+print("Here's your translated text:")
+print(getTranslatedMessage(mode, message, key))
