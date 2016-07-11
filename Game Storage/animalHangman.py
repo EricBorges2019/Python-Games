9066fd09711b00c9c324f2c0e85a47a92df5d6f3
tabuu9 <ericfborges@gmail.com>
2016-07-11 08:52:03 -0400

    1.8.0.1
    Added comments

--- Game Storage/animalHangman.py
+++ Game Storage/animalHangman.py
@@ -3,6 +3,7 @@
 
 def longLine():
 	print('-------------------------------------------------------')
+# fancy spacing line. does nothing important.
 def wait(delayTime):
 	time.sleep(delayTime)
 req_version = (3,0)
@@ -15,7 +16,7 @@ if cur_version < req_version:
 	wait(2)
 	print("									   ")
 	quit()
-# check for correct version of Python
+# a check for correct version of Python
 HANGMANPICS = ['''
 
   +---+
