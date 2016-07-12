8180b1f6ba593d713021d72bb1a2a52edec58311
tabuu9 <ericfborges@gmail.com>
2016-07-12 09:34:45 -0400

    Attempting an import for Version Check

--- Game Storage/dragon.py
+++ Game Storage/dragon.py
@@ -1,21 +1,5 @@
-import time; import random; import sys;
+import time; import random; import sys; import vcheck
 # import for later code
-
-def longLine():
-	print('-------------------------------------------------------')
-def wait(delayTime):
-	time.sleep(delayTime)
-req_version = (3,0)
-cur_version = sys.version_info
-
-if cur_version < req_version:
-
-	print("You are using an outdated version of Python.")
-	longLine()
-	wait(2)
-	print("									   ")
-	quit()
-# check for correct version of Python
 
 def displayIntro():
     print('You are in a land full of dragons.')
