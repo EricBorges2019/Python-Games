93cd1b5b6f7098a79e27e6747b0320d23304c167
EricBorges2019 <ericfborges@gmail.com>
2020-05-18 10:52:07 -0400

    removing redundant code by importing game.py

--- guess.py
+++ guess.py
@@ -1,12 +1,12 @@
-import time; import random; import sys;
+import time; import random; import sys; import game
 # import for later code
 
-def longLine():
-	print('-------------------------------------------------------')
-def wait(delayTime):
-	time.sleep(delayTime)
-req_version = (3,0)
-cur_version = sys.version_info
+# def longLine():
+# 	print('-------------------------------------------------------')
+# def wait(delayTime):
+# 	time.sleep(delayTime)
+# req_version = (3,0)
+# cur_version = sys.version_info
 
 if cur_version < req_version:
 
