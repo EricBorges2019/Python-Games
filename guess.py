6f7004c78a919313185d60b91f0c7150cc61ba51
EricBorges2019 <ericfborges@gmail.com>
2020-05-18 11:03:31 -0400

    Update guess.py
    Fixed borked stuff, now system check should work right

--- guess.py
+++ guess.py
@@ -1,12 +1,9 @@
-import time; import random; import sys; import game
+import time; import random; import sys
 # import for later code
 
-# def longLine():
-# 	print('-------------------------------------------------------')
-# def wait(delayTime):
-# 	time.sleep(delayTime)
-# req_version = (3,0)
-# cur_version = sys.version_info
+
+req_version = (3,0)
+cur_version = sys.version_info
 
 if cur_version < req_version:
 
